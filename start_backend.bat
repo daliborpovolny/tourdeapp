5a010446fb76e193df87c1eb136fb02cb5e24159
OBEREZD <65860022+OBEREZD@users.noreply.github.com>
2025-11-18 18:48:19 +0100

    Simplified .bat & .sh
    The .db file is really starting to get on my nerves, I can't get Git to ignore it, no matter how much it is present in .gitignore!

--- start_backend.bat
+++ start_backend.bat
@@ -1,13 +1,11 @@
 @echo off
 echo Running backend...
 
-for %%a in ("%CD%") do (
-    if /i "%%~nxa"=="TdA26-Goabuc" (
-        if exist .\apps\server\cmd\tourbackend (
-            cd .\apps\server\cmd\tourbackend
-            go run .
-        )
-    )
+
+if exist .\apps\server\cmd\tourbackend (
+    cd .\apps\server\cmd\tourbackend
+    go run .
 )
+
 
 exit /b
--- start_backend.sh
+++ start_backend.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo Running backend...
+
 path_to_main="./apps/server/cmd/tourbackend"
 
-if [[ "${PWD%/}" == */"TdA26-Goabuc" ]]; then
-    if [[ -d "$path_to_main" ]]; then
-        (
-            cd "$path_to_main"
-            go run .
-        )
-    else
-        echo "Path $path_to_main does not exist or isn't a directory."
-        exit 1
-    fi
-else
-    echo "PWD is $PWD, want */tourdeapp."
-    exit 1
+if [[ -d "$path_to_main" ]]; then
+    (
+        cd "$path_to_main"
+        go run .
+    )
 fi
+
 exit 0
