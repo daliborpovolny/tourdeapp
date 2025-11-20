e1c02828b474f77a73c0a1351d60da1e321bdd1a
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-11-20 18:46:26 +0100

    add page of all courses and course detail - that shows just the uuid for now

---
+++ apps/web/src/routes/courses/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+
+    let coursesPromise: Promise<any[]> = loadCourses()
+
+    async function loadCourses() {
+        return fetch("api/courses", {
+            method: 'GET',
+            headers: {'Content-type': 'application/json'}
+        }).then(async (res) => {
+            if (!res.ok) {
+				const err = await res.json()
+				throw new Error(err.message || "Login failed")                
+            }
+            return res.json()
+        }).then((data) => {
+            return data
+        })
+    }
+
+</script>
+
+<h1>Courses</h1>
+<br>
+{#await coursesPromise}
+    <p>Loading</p>
+{:then data}
+    <ul>
+        {#each data as course}
+            <a href="/courses/{course.uuid}"> { course.name } </a>
+            <p> { course.description } </p>
+            <br>
+        {/each}
+    </ul>
+{:catch error}
+    <p></p>
+    
+{/await}
---
+++ apps/web/src/routes/courses/[uuid]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page } from '$app/state';
+</script>
+
+<div>
+    <h1> UUID {page.params.uuid} </h1>
+</div>
