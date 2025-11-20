a79f3a52f21b79f4ef6893ae937d0c2578a9f10d
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-11-20 13:20:54 +0100

    add frontend .sh

--- start_frontend.sh
+++ start_frontend.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo Running backend...
+echo Running frontend...
 
-path_to_back="$PWD/apps/server/cmd/tourbackend"
+path_to_back="$PWD/apps/web"
 
 if [[ -d "$path_to_back" ]]; then
     (
         cd "$path_to_back"
-        go run .
+        npm run dev
     )
 fi
 
