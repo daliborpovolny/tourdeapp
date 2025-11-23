5c991fa0a52fc96e1b203e8386c713b892266145
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-11-24 00:08:22 +0100

    format edit page

--- apps/web/src/routes/edit/courses/+page.svelte
+++ apps/web/src/routes/edit/courses/+page.svelte
@@ -1,28 +1,22 @@
 <script>
-    let course_name = ""
-    let course_description = ""
+	let course_name = '';
+	let course_description = '';
 
-
-
-    function createCourse() {
-        console.log(course_name, course_description)
-    }
-
+	function createCourse() {
+		console.log(course_name, course_description);
+	}
 </script>
 
-
-
 <div>
-    <h1>Create new course</h1>
+	<h1>Create new course</h1>
 
-    <form on:submit={createCourse}>
-        <label for="course_name">Name</label><br>
-        <input type="text" bind:value={course_name} name="course_name"><br>
+	<form on:submit={createCourse}>
+		<label for="course_name">Name</label><br />
+		<input type="text" bind:value={course_name} name="course_name" /><br />
 
-        <label for="course_description">Description</label><br>
-        <input type="text" bind:value={course_description} name="course_description"><br>
+		<label for="course_description">Description</label><br />
+		<input type="text" bind:value={course_description} name="course_description" /><br />
 
-        <button type="submit">Create</button>
-    </form>
-
+		<button type="submit">Create</button>
+	</form>
 </div>
